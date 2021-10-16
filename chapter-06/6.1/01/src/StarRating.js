4734e757de74acd9e2a6f09d5cd51fbad8641307
y4shiro <y4shironao@gmail.com>
2021-10-16 18:08:24 +0900

    StarRating コンポーネント追加

---
+++ chapter-06/6.1/01/src/StarRating.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FaStar } from 'react-icons/fa';
+
+export default function StarRating() {
+  return (
+    <>
+      <FaStar color="red" />
+      <FaStar color="red" />
+      <FaStar color="red" />
+      <FaStar color="grey" />
+      <FaStar color="grey" />
+    </>
+  );
+}
