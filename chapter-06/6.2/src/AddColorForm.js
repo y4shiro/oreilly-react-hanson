7b925465f52b9578b268753d7e492218dbc517df
y4shiro <y4shironao@gmail.com>
2021-10-17 19:13:55 +0900

    AddColorForm コンポーネントを React で制御されていない実装で追加

---
+++ chapter-06/6.2/src/AddColorForm.js
@@ -0,0 +1,28 @@
+import React, { useRef } from 'react';
+
+export default function AddColorForm({ onNewColor = (f) => f }) {
+  const txtTitle = useRef();
+  const hexColor = useRef();
+
+  const submit = (e) => {
+    e.preventDefautl();
+    const title = txtTitle.current.value;
+    const color = hexColor.current.value;
+    onNewColor(title, color);
+    txtTitle.current.value = '';
+    hexColor.current.value = '';
+  };
+
+  return (
+    <form onSubmit={submit}>
+      <input
+        ref={txtTitle}
+        type="text"
+        placeholder="color title ..."
+        required
+      />
+      <input ref={hexColor} type="color" required />
+      <button>ADD</button>
+    </form>
+  );
+}
