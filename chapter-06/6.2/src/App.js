bb7144b2ecb778dedfba3ce4b2ec07da2a6c71e0
y4shiro <y4shironao@gmail.com>
2021-10-17 17:48:20 +0900

    色見本アプリを一旦実装

--- chapter-06/6.2/src/App.js
+++ chapter-06/6.2/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import colorData from './color-data.json';
+import ColorList from './ColorList';
 
 function App() {
   const [colors] = useState(colorData);
-  return <div className="App"></div>;
+  return <ColorList colors={colors} />;
 }
 
 export default App;
---
+++ chapter-06/6.2/src/StarRating.js
@@ -0,0 +1,14 @@
+import Star from './Star';
+
+export default function StarRating({ totalStars = 5, selectedStars = 0 }) {
+  return (
+    <>
+      {[...Array(totalStars)].map((n, i) => (
+        <Star key={i} selected={selectedStars > i} />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </>
+  );
+}
