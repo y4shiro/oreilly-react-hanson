6ac33070b22d629f8eb136e9a3e712e29adee6b1
y4shiro <y4shironao@gmail.com>
2021-10-17 14:50:20 +0900

    StarRating が親から props でスタイルを渡された場合に反映できるよう実装

--- chapter-06/6.1/01/src/StarRating.js
+++ chapter-06/6.1/01/src/StarRating.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 
 import Star from './Star';
 
-export default function StarRating({ totalStars = 5 }) {
+export default function StarRating({ style = {}, totalStars = 5 }) {
   const [selectedStars, setSelectedStars] = useState(0);
 
   return (
-    <>
+    <div style={{ padding: '5px', ...style }}>
       {[...Array(totalStars)].map((n, i) => (
         <Star
           key={i}
@@ -17,6 +17,6 @@ export default function StarRating({ totalStars = 5 }) {
       <p>
         {selectedStars} of {totalStars} stars
       </p>
-    </>
+    </div>
   );
 }
