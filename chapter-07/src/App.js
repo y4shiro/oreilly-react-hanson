4b0abcf45a2e532f64e379295b812a9469a02b61
y4shiro <y4shironao@gmail.com>
2021-10-18 17:55:59 +0900

    配列の値をメモ化することで、余計な副作用の実行を抑える

--- chapter-07/src/App.js
+++ chapter-07/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 const useAnyKeyToRender = () => {
   const [, forceRender] = useState();
@@ -9,12 +9,25 @@ const useAnyKeyToRender = () => {
   }, []);
 };
 
-export default function App() {
+function WordCount({ children = '' }) {
   useAnyKeyToRender();
+
+  const words = useMemo(() => children.split(' '), [children]);
 
   useEffect(() => {
     console.log('fresh render');
-  });
+  }, [words]);
 
-  return <h1>Open the console</h1>;
+  return (
+    <>
+      <p>{children}</p>
+      <p>
+        <strong>{words.length} - words</strong>
+      </p>
+    </>
+  );
+}
+
+export default function App() {
+  return <WordCount>You are not going to believe this but...</WordCount>;
 }
