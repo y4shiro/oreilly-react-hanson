fa4766c1f3182025cfed6b2d8da4b82d02a88d96
y4shiro <y4shironao@gmail.com>
2021-10-18 18:44:11 +0900

    ブラウザのウィンドウサイズを画面描画前に useLayoutEffect で取得する

--- chapter-07/7.2/src/App.js
+++ chapter-07/7.2/src/App.js
@@ -1,9 +1,30 @@
-import React, { useEffect, useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
+
+function useWindowSize() {
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  const resize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener('resize', resize);
+    resize();
+    return () => window.removeEventListener('resize', resize);
+  }, []);
+
+  return [width, height];
+}
 
 function App() {
-  useEffect(() => console.log('useEffect'));
-  useLayoutEffect(() => console.log('useLayoutEffect'));
-  return <div>ready</div>;
+  const [w, h] = useWindowSize();
+  return (
+    <div>
+      {w}x{h}
+    </div>
+  );
 }
 
 export default App;
