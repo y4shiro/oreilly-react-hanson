b56cfc535e1a8013ef8d67607aa1118db9b89140
y4shiro <y4shironao@gmail.com>
2021-10-19 17:18:19 +0900

    predicate ではなく、useCallback で再描画の抑制を行った例

--- chapter-07/7.4/src/App.js
+++ chapter-07/7.4/src/App.js
@@ -1,25 +1,22 @@
-import React, { useState, memo } from 'react';
+import React, { useState, useCallback, memo } from 'react';
 
 const Cat = ({ name, meow = (f) => f }) => {
   console.log(`rendering ${name}`);
   return <p onClick={() => meow(name)}>{name}</p>;
 };
 
-const PureCat = memo(
-  Cat,
-  (prevProps, nextProps) => prevProps.name === nextProps.name
-);
+const PureCat = memo(Cat);
 
 function App() {
   const [cats, setCats] = useState(['Biscuit', 'Jungle', 'Oullaw']);
+  const meow = useCallback((name) => {
+    console.log(`${name} has meowd`);
+  }, []);
+
   return (
     <>
       {cats.map((name, i) => (
-        <PureCat
-          key={i}
-          name={name}
-          meow={(name) => console.log(`${name} has meowd`)}
-        />
+        <PureCat key={i} name={name} meow={meow} />
       ))}
       <button onClick={() => setCats([...cats, prompt('Name a cat')])}>
         Add a Cat
