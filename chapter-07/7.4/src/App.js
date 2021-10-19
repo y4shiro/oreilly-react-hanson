9a7174449122e0feaec7363203393a8bb4837f70
y4shiro <y4shironao@gmail.com>
2021-10-19 16:22:12 +0900

    猫の一覧を表示、追加するアプリ

--- chapter-07/7.4/src/App.js
+++ chapter-07/7.4/src/App.js
@@ -1,5 +1,22 @@
+import React, { useState } from 'react';
+
+const Cat = ({ name }) => {
+  console.log(`rendering ${name}`);
+  return <p>{name}</p>;
+};
+
 function App() {
-  return <div>hello</div>;
+  const [cats, setCats] = useState(['Biscuit', 'Jungle', 'Oullaw']);
+  return (
+    <>
+      {cats.map((name, i) => (
+        <Cat key={i} name={name} />
+      ))}
+      <button onClick={() => setCats([...cats, prompt('Name a cat')])}>
+        Add a Cat
+      </button>
+    </>
+  );
 }
 
 export default App;
