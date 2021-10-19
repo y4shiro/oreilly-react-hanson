78ef91715ff066f799b792fedecbc2399b17ca6e
y4shiro <y4shironao@gmail.com>
2021-10-19 17:13:07 +0900

    Cat コンポーネントに関数プロパティが存在する場合は、memo 関数の第 2 引数に predicate を指定することで再描画の挙動を指定できる

--- chapter-07/7.4/src/App.js
+++ chapter-07/7.4/src/App.js
@@ -1,18 +1,25 @@
 import React, { useState, memo } from 'react';
 
-const Cat = ({ name }) => {
+const Cat = ({ name, meow = (f) => f }) => {
   console.log(`rendering ${name}`);
-  return <p>{name}</p>;
+  return <p onClick={() => meow(name)}>{name}</p>;
 };
 
-const PureCat = memo(Cat);
+const PureCat = memo(
+  Cat,
+  (prevProps, nextProps) => prevProps.name === nextProps.name
+);
 
 function App() {
   const [cats, setCats] = useState(['Biscuit', 'Jungle', 'Oullaw']);
   return (
     <>
       {cats.map((name, i) => (
-        <PureCat key={i} name={name} />
+        <PureCat
+          key={i}
+          name={name}
+          meow={(name) => console.log(`${name} has meowd`)}
+        />
       ))}
       <button onClick={() => setCats([...cats, prompt('Name a cat')])}>
         Add a Cat
