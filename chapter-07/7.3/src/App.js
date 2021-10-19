5186de7877845c915a4c886213137f4c8370d024
y4shiro <y4shironao@gmail.com>
2021-10-19 11:22:03 +0900

    checkbox の更新に useReducer を使うことでロジックを抽象化

--- chapter-07/7.3/src/App.js
+++ chapter-07/7.3/src/App.js
@@ -1,15 +1,11 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 function Checkbox() {
-  const [checked, setChecked] = useState(false);
+  const [checked, toggle] = useReducer((checked) => !checked, false);
 
   return (
     <>
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={() => setChecked((checked) => !checked)}
-      />
+      <input type="checkbox" value={checked} onChange={toggle} />
       {checked ? 'checked' : 'not checked'}
     </>
   );
