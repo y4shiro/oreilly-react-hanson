23db816f2108f9590c5198646540fb8f1866c9fb
y4shiro <y4shironao@gmail.com>
2021-10-19 11:10:55 +0900

    Checkbox コンポーネント追加

--- chapter-07/7.3/src/App.js
+++ chapter-07/7.3/src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <div>hello</div>;
+import React, { useState } from 'react';
+
+function Checkbox() {
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={() => setChecked((checked) => !checked)}
+      />
+      {checked ? 'checked' : 'not checked'}
+    </>
+  );
 }
 
-export default App;
+export default function App() {
+  return <Checkbox />;
+}
