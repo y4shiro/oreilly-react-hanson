38637d031894b0e16f2f3628561232062de6b855
y4shiro <y4shironao@gmail.com>
2021-10-19 14:58:24 +0900

    useState でステートの特定プロパティのみ上書きする

--- chapter-07/7.3.1/src/App.js
+++ chapter-07/7.3.1/src/App.js
@@ -1,5 +1,40 @@
+import React, { useState } from 'react';
+
+const firstUser = {
+  id: '0391-3233-3201',
+  firstName: 'Bill',
+  lastName: 'Wilson',
+  city: 'Missoula',
+  state: 'Montana',
+  email: 'bwilson@mtnwilsons.com',
+  admin: false,
+};
+
+function User() {
+  const [user, setUser] = useState(firstUser);
+
+  return (
+    <div>
+      <h1>
+        {user.firstName} {user.lastName} - {user.admin ? 'Admin' : 'User'}
+      </h1>
+      <p>Email: {user.email}</p>
+      <p>
+        Location: {user.city}, {user.state}
+      </p>
+      <button
+        onClick={() => {
+          setUser({ ...user, admin: true });
+        }}
+      >
+        Make Admin
+      </button>
+    </div>
+  );
+}
+
 function App() {
-  return <div>hello</div>;
+  return <User />;
 }
 
 export default App;
