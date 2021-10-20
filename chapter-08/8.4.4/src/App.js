b26e1d1fe8ea40acaea86e39b96e3f5c1a3ae60b
y4shiro <y4shironao@gmail.com>
2021-10-20 18:02:29 +0900

    Fetch コンポーネントに fetch の処理や UI を統合して使い回せるようにした

--- chapter-08/8.4.4/src/App.js
+++ chapter-08/8.4.4/src/App.js
@@ -1,5 +1,34 @@
+import React from 'react';
+import Fetch from './Fetch';
+
+function GitHubUser({ login }) {
+  return (
+    <Fetch
+      uri={`https://api.github.com/users/${login}`}
+      renderSuccess={UserDetails}
+    />
+  );
+}
+
+function UserDetails({ data }) {
+  return (
+    <div className="githubUser">
+      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+  );
+}
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <GitHubUser login="moonhighway" />
+    </>
+  );
 }
 
 export default App;
