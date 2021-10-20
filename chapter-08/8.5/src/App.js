188225d52f09bfaac0663f886b313a3be1a86fc7
y4shiro <y4shironao@gmail.com>
2021-10-20 20:14:59 +0900

    GitHub からユーザーのリポジトリ情報を取得して表示

--- chapter-08/8.5/src/App.js
+++ chapter-08/8.5/src/App.js
@@ -1,5 +1,13 @@
-function App() {
-  return <div>hello</div>;
-}
+import React, { useState } from 'react';
+import SearchForm from './SearchForm';
+import GitHubUser from './GitHubUser';
 
-export default App;
+export default function App() {
+  const [login, setLogin] = useState('moonhighway');
+  return (
+    <>
+      <SearchForm value={login} onSearch={setLogin} />
+      <GitHubUser login={login} />
+    </>
+  );
+}
