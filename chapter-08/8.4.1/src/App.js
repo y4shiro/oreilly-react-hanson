31a09dc0cc5a4409ce6b50ea045208a0c02f46c6
y4shiro <y4shironao@gmail.com>
2021-10-20 16:00:34 +0900

    5,000 件のデータをそのままリストとして描画するとリソースをかなり食ってしまう

--- chapter-08/8.4.1/src/App.js
+++ chapter-08/8.4.1/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import faker from 'faker';
 
-const tahoe_peaks = [
-  { name: 'Freel Peak', elevation: 10891 },
-  { name: 'Monument Peak', elevation: 10067 },
-  { name: 'Pyramid Peak', elevation: 9983 },
-  { name: 'Mt. Tallac', elevation: 9735 },
-];
+const bigList = [...Array(5000)].map(() => ({
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  avatar: faker.internet.avatar(),
+}));
 
 function List({ data = [], renderEmpty, renderItem }) {
   return !data.length ? (
@@ -20,15 +20,14 @@ function List({ data = [], renderEmpty, renderItem }) {
 }
 
 export default function App() {
-  return (
-    <List
-      data={tahoe_peaks}
-      renderEmpty={<p>This list is empty</p>}
-      renderItem={(item) => (
-        <>
-          {item.name} - {item.elevation.toLocaleString()}
-        </>
-      )}
-    />
+  const renderItem = (item) => (
+    <div style={{ display: 'flex' }}>
+      <img src={item.avatar} alt={item.name} width={50} />
+      <p>
+        {item.name} - {item.email}
+      </p>
+    </div>
   );
+
+  return <List data={bigList} renderItem={renderItem} />;
 }
