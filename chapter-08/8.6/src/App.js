14c1338d69b5b2cf3a0ac4b2e329c9397edbd2c3
y4shiro <y4shironao@gmail.com>
2021-10-21 20:18:37 +0900

    GitHub の GraphQL API でユーザ情報を取得

--- chapter-08/8.6/src/App.js
+++ chapter-08/8.6/src/App.js
@@ -1,5 +1,77 @@
-function App() {
-  return <div>hello</div>;
+import React, { useState, useEffect } from 'react';
+import SearchForm from './SearchForm';
+import { GraphQLClient } from 'graphql-request';
+
+const query = `
+  query findRepos($login:String!) {
+    user(login:$login) {
+      login
+      name
+      location
+      avatar_url: avatarUrl
+      repositories(first:100) {
+        totalCount
+        nodes {
+          name
+        }
+      }
+    }
+  }
+`;
+
+const client = new GraphQLClient('https://api.github.com/graphql', {
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+  },
+});
+
+function UserDetails(data) {
+  return (
+    <div className="githubUser">
+      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+  );
 }
 
-export default App;
+function List({ data = [], renderItem, renderEmpty }) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item, i) => (
+        <li key={i}>{renderItem(item)}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default function App() {
+  const [login, setLogin] = useState('moontahoe');
+  const [userData, setUserData] = useState();
+
+  useEffect(() => {
+    client
+      .request(query, { login })
+      .then(({ user }) => user)
+      .then(setUserData)
+      .catch(console.error);
+  }, [client, query, login]);
+
+  if (!userData) return <p>loading...</p>;
+  console.log(userData);
+  return (
+    <>
+      <SearchForm value={login} onSearch={setLogin} />
+      <UserDetails {...userData} />
+      <List
+        data={userData.repositories.nodes}
+        renderItem={(repo) => <span>{repo.name}</span>}
+      />
+    </>
+  );
+}
