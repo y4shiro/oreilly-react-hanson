e26740f36782f1b1d835d29d86eb602262ecc768
y4shiro <y4shironao@gmail.com>
2021-10-21 15:52:29 +0900

    リポジトリのデータ取得を App コンポーネント経由で実行

--- chapter-08/8.5/src/RepoMenu.js
+++ chapter-08/8.5/src/RepoMenu.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useIterator } from './hooks';
-import RepositoryReadme from './RepositoryReadme';
 
-export default function RepoMenu({ repositories, login }) {
-  const [{ name }, previous, next] = useIterator(repositories);
+export default function RepoMenu({
+  repositories,
+  selected,
+  onSelect = (f) => f,
+}) {
+  const [{ name }, previous, next] = useIterator(
+    repositories,
+    selected
+      ? repositories.findIndex((repo) => repo.name === selected)
+      : undefined
+  );
+
+  useEffect(() => {
+    if (!name) return;
+    onSelect(name);
+  }, [name]);
 
   return (
     <>
@@ -12,7 +25,6 @@ export default function RepoMenu({ repositories, login }) {
         <p>{name}</p>
         <button onClick={next}>&gt;</button>
       </div>
-      <RepositoryReadme login={login} repo={name} />
     </>
   );
 }
--- chapter-08/8.5/src/UserRepositories.js
+++ chapter-08/8.5/src/UserRepositories.js
@@ -2,12 +2,20 @@ import React from 'react';
 import Fetch from './Fetch';
 import RepoMenu from './RepoMenu';
 
-export default function UserRepositories({ login, onSelect = (f) => f }) {
+export default function UserRepositories({
+  login,
+  selectedRepo,
+  onSelect = (f) => f,
+}) {
   return (
     <Fetch
       uri={`https://api.github.com/users/${login}/repos`}
       renderSuccess={({ data }) => (
-        <RepoMenu login={login} repositories={data} onSelect={onSelect} />
+        <RepoMenu
+          repositories={data}
+          selected={selectedRepo}
+          onSelect={onSelect}
+        />
       )}
     />
   );
