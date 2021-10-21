cfbb96518a4ca38d797317a1f0e63e4e86e3042c
y4shiro <y4shironao@gmail.com>
2021-10-21 21:33:50 +0900

    エラーバウンダリ実装

--- chapter-09/9.1/src/App.js
+++ chapter-09/9.1/src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import SiteLayout from './SiteLayout.js';
+import SiteLayout from './SiteLayout';
+import ErrorBoundary, { BreakThings } from './ErrorBoundary';
 
 const Callout = ({ children }) => <div className="callout">{children}</div>;
 
 export default function App() {
   return (
-    <SiteLayout menu={<p>Menu</p>}>
-      <>
+    <SiteLayout
+      menu={
+        <ErrorBoundary>
+          <p>Menu</p>
+        </ErrorBoundary>
+      }
+    >
+      <ErrorBoundary>
         <Callout>Callout</Callout>
+      </ErrorBoundary>
+      <ErrorBoundary>
         <h1>Contents</h1>
-        <p>This is hte main part of the example layout</p>
-      </>
+        <p>this is the main part of the example layout</p>
+        <BreakThings />
+      </ErrorBoundary>
     </SiteLayout>
   );
 }
