51a2650f6bf38cd6f75dbcd6da9fc7dbdeb1177e
y4shiro <y4shironao@gmail.com>
2021-10-21 20:41:14 +0900

    SiteLayout コンポーネントの追加

--- chapter-09/9.1/src/App.js
+++ chapter-09/9.1/src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <div>hello</div>;
-}
+import React from 'react';
+import SiteLayout from './SiteLayout.js';
 
-export default App;
+const Callout = ({ children }) => <div className="callout">{children}</div>;
+
+export default function App() {
+  return (
+    <SiteLayout menu={<p>Menu</p>}>
+      <>
+        <Callout>Callout</Callout>
+        <h1>Contents</h1>
+        <p>This is hte main part of the example layout</p>
+      </>
+    </SiteLayout>
+  );
+}
