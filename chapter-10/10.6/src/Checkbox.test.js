6cc6051c1b1ee6dd734080de97625c3f51381def
y4shiro <y4shironao@gmail.com>
2021-10-22 19:50:14 +0900

    @testing-library/react にて、data 属性を用いて DOM の参照を取得した例

--- chapter-10/10.6/src/Checkbox.test.js
+++ chapter-10/10.6/src/Checkbox.test.js
@@ -3,8 +3,9 @@ import { render, fireEvent } from '@testing-library/react';
 import { Checkbox } from './Checkbox';
 
 test('Selecting the checkbox should toggle its value', () => {
-  const { getByLabelText } = render(<Checkbox />);
-  const checkbox = getByLabelText(/not checked/);
+  const { getByLabelText, getByTestId } = render(<Checkbox />);
+  // const checkbox = getByLabelText(/not checked/);
+  const checkbox = getByTestId('checkbox');
   fireEvent.click(checkbox);
   expect(checkbox.checked).toEqual(true);
   fireEvent.click(checkbox);
