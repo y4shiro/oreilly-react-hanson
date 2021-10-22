e6d5c1432ca23d369e722ea184d947fbb8d2fba4
y4shiro <y4shironao@gmail.com>
2021-10-22 19:32:44 +0900

    @testing-library/react の render 関数を用いて要素を検索

--- chapter-10/10.6/src/Star.test.js
+++ chapter-10/10.6/src/Star.test.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Star from './Star';
 import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
 
 test('renders a star', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Star />, div);
   expect(div.querySelector('svg')).toHaveAttribute('id', 'star');
+});
+
+test('renders an h1', () => {
+  const { getByText } = render(<Star />);
+  const h1 = getByText(/Great Star/);
+  expect(h1).toHaveTextContent('Great Star');
 });
