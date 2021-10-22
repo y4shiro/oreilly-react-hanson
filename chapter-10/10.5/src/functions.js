a7bb65c5add2014e6f40bbb03a763c9c4e40e89b
y4shiro <y4shironao@gmail.com>
2021-10-22 18:16:01 +0900

    order 関数のテストを記述、マッチャーとして toEqual を使用した

--- chapter-10/10.5/src/functions.js
+++ chapter-10/10.5/src/functions.js
@@ -1,3 +1,8 @@
 export function timesTwo(a) {
   return a * 2;
 }
+
+export function order(items) {
+  const total = items.reduce((price, item) => price + item.price, 0);
+  return { orderItems: items, total };
+}
