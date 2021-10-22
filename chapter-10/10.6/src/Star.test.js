e64f0d7ffcb83a26120c5257559af284ac051247
y4shiro <y4shironao@gmail.com>
2021-10-22 19:07:12 +0900

    jest-dom を使用してテストを記述

--- chapter-10/10.6/src/Star.test.js
+++ chapter-10/10.6/src/Star.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Star from './Star';
+import '@testing-library/jest-dom';
 
 test('renders a star', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Star />, div);
-  expect(div.querySelector('svg')).toBeTruthy();
+  expect(div.querySelector('svg')).toHaveAttribute('id', 'star');
 });
