599beadefbede02999b0285685556493df068c30
y4shiro <y4shironao@gmail.com>
2021-10-23 17:40:37 +0900

    React Router を App コンポーネントに組み込み

--- chapter-11/11.1/src/App.js
+++ chapter-11/11.1/src/App.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Home, About, Events, Products, Contact } from './pages';
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- chapter-11/11.1/src/index.js
+++ chapter-11/11.1/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+import { BrowserRouter as Router } from 'react-router-dom';
+
+render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
