0c4716f58837468fbe2a7385bf5f1e3b55152e00
y4shiro <y4shironao@gmail.com>
2021-10-24 14:29:04 +0900

    12.2 のプロジェクト作成

---
+++ chapter-12/12.2/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Menu } from './Menu';
+import data from './data.json';
+
+ReactDOM.hydrate(
+  <Menu recipes={data} title="Delicious Recipes" />,
+  document.getElementById('root')
+);
